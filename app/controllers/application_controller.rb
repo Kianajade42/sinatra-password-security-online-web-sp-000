3d449bfb9e9babe151de369e2b6252e8fa5321d1
Kiana Long <kianajade42.kl@gmail.com>
2021-03-20 16:35:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,17 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
-		user=User.new(:username =>
-	params[:username], :password =>
-	params[:password])
-	end
+  	user=User.find_by(:username =>
+ 	params[:username])
+ 	if user&&
+ 		user.authenticate(params[:password])
+     session[:user_id] = user.id
+ 		redirect "/success"
+ 	else
+ 		redirect "/failure"
+ 	end
+  end
+
 
 	get "/login" do
 		erb :login
@@ -39,18 +45,7 @@ class ApplicationController < Sinatra::Base
 	end
 end
 
- post "/login" do
- 	user=User.find_by(:username =>
-	params[:username])
-	if user&&
-		user.authenticate(params[:password])
-    session[:user_id] = user.id
-		redirect "/success"
-	else
-		redirect "/failure"
-	end
- end
-
+ 
 	get "/success" do
 		if logged_in?
 			erb :success
